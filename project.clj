71bacf5ffa48e2a023d1238cc3efbc1e52d987a1
Lee Hinman <lee@writequit.org>
2019-08-22 16:15:35 -0600

    Spit out more things

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :repositories [["crosswire" "https://crosswire.org/mvn/content/groups/public/"]]
   :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/data.zip "0.1.3"]
                  [org.crosswire/jsword "2.1-SNAPSHOT"]
                  [cheshire "5.9.0"]
                  [clj-http "3.10.0"]]
--- src/stepsearch/core.clj
+++ src/stepsearch/core.clj
@@ -1,22 +1,34 @@
 (ns stepsearch.core
   (:gen-class)
-  (:require [clojure.java.io :as io])
+  (:require [clojure.java.io :as io]
+            [clojure.data.zip.xml :refer (attr text xml->)]
+            [clojure.xml :as xml]
+            [clojure.zip :as zip])
   (:import (java.net URI)
+           (java.io ByteArrayInputStream)
            (org.crosswire.jsword.book.sword BlockType
                                             SwordBook
                                             SwordBookMetaData
-                                            ZVerseBackend)))
+                                            ZVerseBackend)
+           (org.xml.sax SAXParseException)))
 
 (defn -main
-  "I don't do a whole lot."
+  "This function is entirely exploratory. Don't use it."
   [& args]
   (let [f (io/file (io/resource "esv2016/mods.d/ESV2016_th.conf"))
         meta (SwordBookMetaData. f (.toURI (io/resource "esv2016")))
-        ;; BlockType blockType = BlockType.fromString(sbmd.getProperty(SwordBookMetaData.KEY_BLOCK_TYPE))
         blockType (BlockType/fromString (.getProperty meta SwordBookMetaData/KEY_BLOCK_TYPE))
         datasize 2
         zversebackend (ZVerseBackend. meta blockType datasize)
         book (SwordBook. meta zversebackend)
         keylist (.getGlobalKeyList book)]
-    (doseq [k keylist]
-      (println (.getRawText book k)))))
+    (doseq [k (take 5 keylist)]
+      (let [text (str "<doc>" (.getRawText book k) "</doc>") ;; wrap because otherwise invalid XML :(
+            xml (try
+                  (xml/parse (ByteArrayInputStream. (.getBytes text "UTF-8")))
+                  (catch SAXParseException e
+                    "_malformed_"))
+            zipped (zip/xml-zip xml)]
+        (println text)
+        (println (:content xml))
+        (println (:content (first zipped)))))))
